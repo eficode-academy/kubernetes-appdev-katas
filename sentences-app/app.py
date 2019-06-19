692448ce3434172cdbb12a60c004e8c0997769f6
MichaelVL <mvl.gh@network42.dk>
2019-06-19 13:38:47 +0200

    Add sentence application

---
+++ sentences-app/app.py
@@ -0,0 +1,46 @@
+
+import argparse
+import flask
+import random
+import requests
+
+age_app = flask.Flask('age')
+name_app = flask.Flask('name')
+sentence_app = flask.Flask('sentence')
+
+@age_app.route('/')
+def get_age():
+    return str(random.randint(0,100))
+
+@name_app.route('/')
+def get_name():
+    return random.choice(['Graham', 'John', 'Terry', 'Eric', 'Michael'])
+
+@sentence_app.route('/')
+def get_sentence():
+    age = requests.get(sentence_app.config['age-service']).text
+    name = requests.get(sentence_app.config['name-service']).text
+    return '{} is {} years'.format(name, age)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', choices=['sentence', 'age', 'name'], help="Select role for the program")
+    parser.add_argument('--delay', default=0, type=int)
+    parser.add_argument('--age-service', default='http://127.0.0.1:8888')
+    parser.add_argument('--name-service', default='http://127.0.0.1:8889')
+    parser.add_argument('--server-address', default='0.0.0.0:5000')
+
+    args = parser.parse_args()
+
+    host, port = args.server_address.split(':')
+    port = int(port)
+
+    if args.mode=='age':
+        age_app.run(host=host, port=port)
+    elif args.mode=='name':
+        name_app.run(host=host, port=port)
+    elif args.mode=='sentence':
+        sentence_app.config['name-service'] = args.name_service
+        sentence_app.config['age-service'] = args.age_service
+        sentence_app.run(host=host, port=port)
