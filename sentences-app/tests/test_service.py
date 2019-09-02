68db785ef95dddbd5770cab83cd24d50c267fb3c
MichaelVL <mvl.gh@network42.dk>
2019-09-02 15:29:55 +0200

    Editorial

--- sentences-app/tests/test_service.py
+++ sentences-app/tests/test_service.py
@@ -7,10 +7,11 @@ class TestService(unittest.TestCase):
     url = os.getenv('SERVICE_URL', 'http://127.0.0.1:8890')
 
     def test_response_format(self):
-        response = requests.get(self.url, timeout=1)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.encoding, 'utf-8')
-        self.assertTrue(re.match('^\w+ is \d+ years$', response.text))
+        for attempt in range(10):
+            response = requests.get(self.url, timeout=1)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.encoding, 'utf-8')
+            self.assertTrue(re.match('^\w+ is \d+ years$', response.text))
 
 if __name__ == '__main__':
     unittest.main()
