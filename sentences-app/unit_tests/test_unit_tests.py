f5727df64ae4029238380f99d18b7b270a4efb5e
MichaelVL <mvl.gh@network42.dk>
2019-09-03 13:40:19 +0200

    Update unit testing container image and add tests

--- sentences-app/unit_tests/test_unit_tests.py
+++ sentences-app/unit_tests/test_unit_tests.py
@@ -7,11 +7,26 @@ import random
 import app.utils as app
 
 class TestNameUnittest(unittest.TestCase):
-    def test_response_format(self):
+    def test_name_casing(self):
         random.seed(0)
         for attempt in range(10):
             name = app.get_random_name()
             self.assertTrue(re.match('^[A-Z]{1}[a-z]+$', name))
+
+    def test_name_length(self):
+        random.seed(0)
+        for attempt in range(10):
+            name = app.get_random_name()
+            self.assertTrue(len(name) < 20)
+
+    def test_randomness_distribution(self):
+        random.seed(0)
+        got_name = {}
+        for attempt in range(100):
+            name = app.get_random_name()
+            got_name[name] = got_name.get(name, 0)+1
+        self.assertTrue(len(got_name.keys()) > 0) # Expect between 1 and 5 names
+        self.assertTrue(len(got_name.keys()) < 6)
 
 if __name__ == '__main__':
     unittest.main()
