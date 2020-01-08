dc2bc09a3f8ccd1b1d7cf17981a9f329b99bc708
Sofus Albertsen <soa@praqma.com>
2020-01-08 15:27:14 +0100

    Deleted old python app

--- sentences-app/app/utils.py
+++
@@ -1,9 +0,0 @@
-# Utility functions
-
-import random
-
-def get_random_age():
-    return str(random.randint(0,100))
-
-def get_random_name():
-    return random.choice(['Graham', 'John', 'Terry', 'Eric', 'Michael'])
